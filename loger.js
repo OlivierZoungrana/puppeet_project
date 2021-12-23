141e7b3d78212846bd62a86bf96817c3a5498410
OlivierZoungrana <synthejazz@gmail.com>
2021-12-23 09:31:44 +0100

    new

--- loger.js
+++ loger.js
@@ -2,8 +2,9 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('https://seloger.com');
-  await page.screenshot({ path: 'example.png' });
-
-  await browser.close();
+  await page.goto('https:fr.indeed.com/', {waitUntil:'networkidle2'});
+  await page.type('[name=q]', 'javascript', {delay: 100})
+   // await page.type('[name=l]', 'paris', {delay:50})
+   await page.click('button[type=submit]')
+//   await browser.close();
 })();
